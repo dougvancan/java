0321afc26afa552c2f47e6a197cc67091113658a
douglas.vancan <douglas.vancan@SMSP-020606.sysmap.com.br>
2019-07-08 08:01:51 -0300

    handing-exception

---
+++ java-spring-boot-handing-exception/src/main/java/com/course/practicaljava/rest/service/CarService.java
@@ -0,0 +1,18 @@
+package com.course.practicaljava.rest.service;
+
+import java.util.List;
+
+import com.course.practicaljava.rest.domain.Car;
+
+public interface CarService {
+
+	List<String> BRANDS = List.of("Toyota", "Honda", "Ford", "Mitsubishi", "Chevrolet");
+	List<String> COLORS = List.of("Red", "Black", "White", "Blue", "Silver");
+	List<String> TYPES = List.of("Sedan", "SUV", "MPV", "Truck", "Coupe");
+	List<String> ADDITIONAL_FEATURES = List.of("GPS", "Alarm", "Sunroof", "Media player", "Leather seats");
+	List<String> FUEL_TYPES = List.of("Petrol", "Electric", "Hybrid");
+	List<String> TIRE_MANUFACTURERS = List.of("Goodyear", "Bridgestone", "Dunlop");
+
+	Car generateCar();
+
+}
